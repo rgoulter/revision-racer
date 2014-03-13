65041fb69277875ff55297826624e2af1471a1eb
Richard Goulter <richard.goulter@gmail.com>
2014-03-13 17:16:04 +0800

    Improved upon skeleton, (no functionality) for some asteroid stuff.

--- playground/AsteroidHelper/AsteroidHelper/ViewController.h
+++ playground/AsteroidHelper/AsteroidHelper/ViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UIPickerView *generationType;
 
 @end
--- playground/AsteroidHelper/AsteroidHelper/ViewController.m
+++ playground/AsteroidHelper/AsteroidHelper/ViewController.m
@@ -18,6 +18,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +26,31 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)newAsterButtonPressed:(id)sender {
+    
+}
+
+- (IBAction)explodeAsterButtonPressed:(id)sender {
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1; // For one column
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return 3; // Numbers of rows
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    NSArray *array = @[@"Cube",
+                       @"Truncated Icosahedron",
+                       @"Dodecahedron"];
+    return [array objectAtIndex:row]; // If it's a string
+}
+
 
 @end
