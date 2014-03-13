db19d2bd81abab5e3ea6d87c9642d3f40fa56df4
Richard Goulter <richard.goulter@gmail.com>
2014-03-13 13:59:53 +0800

    Copypaste gitignore from PS5

---
+++ playground/AsteroidHelper/AsteroidHelper/AsteroidDemoViewController.m
@@ -0,0 +1,13 @@
+//
+//  AsteroidDemoViewController.m
+//  AsteroidHelper
+//
+//  Created by Richard Goulter on 13/3/14.
+//  Copyright (c) 2014 nus.cs3217. All rights reserved.
+//
+
+#import "AsteroidDemoViewController.h"
+
+@implementation AsteroidDemoViewController
+
+@end
---
+++ playground/AsteroidHelper/AsteroidHelper/StarfieldViewController.m
@@ -0,0 +1,13 @@
+//
+//  StarfieldViewController.m
+//  AsteroidHelper
+//
+//  Created by Richard Goulter on 13/3/14.
+//  Copyright (c) 2014 nus.cs3217. All rights reserved.
+//
+
+#import "StarfieldViewController.h"
+
+@implementation StarfieldViewController
+
+@end
