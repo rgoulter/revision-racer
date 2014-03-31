659ca3b00db6e30eeb9debc36755ec0958a01d20
Richard Goulter <richard.goulter@gmail.com>
2014-03-31 11:26:13 +0800

    Landscape'd the project views.. added some buttons and a label to the game.

--- skeleton/RacerGame/RacerGame/GameViewController.h
+++ skeleton/RacerGame/RacerGame/GameViewController.h
@@ -15,6 +15,14 @@
 @property (strong, nonatomic) EAGLContext *context;
 @property (strong, nonatomic) GLKBaseEffect *effect;
 
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
+@property (weak, nonatomic) IBOutlet UIButton *answerBtn0;
+@property (weak, nonatomic) IBOutlet UIButton *answerBtn1;
+@property (weak, nonatomic) IBOutlet UIButton *answerBtn2;
+@property (weak, nonatomic) IBOutlet UIButton *answerBtn3;
+@property (weak, nonatomic) IBOutlet UIButton *answerBtn4;
+
+
 - (void)setUpGL;
 - (void)tearDownGL;
 
