dbed28471cc233456a1df68e4016df90501980b9
Hunar Khanna <hunur.khanna@gmail.com>
2014-04-03 15:56:46 +0800

    Persistence object declaration and access changed to Resources.h

---
+++ skeleton/RacerGame/RacerGame/Resources.h
@@ -0,0 +1,21 @@
+//
+//  Resources.h
+//  RacerGame
+//
+//  Created by Hunar Khanna on 3/4/14.
+//  Copyright (c) 2014 Hunar Khanna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Resources : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
++ (id)singleton;
+@end
