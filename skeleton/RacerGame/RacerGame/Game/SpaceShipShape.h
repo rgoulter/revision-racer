be2e31a3ab59677cc039bfb9a492602cea0c9f90
Richard Goulter <richard.goulter@gmail.com>
2014-04-13 13:35:47 +0800

    Change-to spaceship, so the colours are less ... vibrant.

--- skeleton/RacerGame/RacerGame/Game/SpaceShipShape.h
+++ skeleton/RacerGame/RacerGame/Game/SpaceShipShape.h
@@ -1,13 +1,5 @@
-//
-//  SpaceShipShape.h
-//  RacerGame
-//
-//  Created by Richard Goulter on 5/4/14.
-//  Copyright (c) 2014 Hunar Khanna. All rights reserved.
-//
-
 #import "Shapes.h"
 
-@interface BOSpaceShipShape : BOShape
 
+@interface BOSpaceShipShape : BOShape
 @end
